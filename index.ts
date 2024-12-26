6dae47c063c1debc3e5af208e2c05b5be869a7c9
sagiajaj <sagiajaj@gmail.com>
2024-12-26 11:59:41 +0200

    Index update security group

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import { config } from "./src/globals/config";
 import { ACMSSLCertificate } from './src/dns/ssl';
 import { HostedZone } from './src/dns/zone';
 import { DNS } from "./src/dns/domains";
-import { mainRedisAllowAllSG } from './src/network/security-groups';
+import { mainRedisAllowAllSG, mainSecurityGroup } from './src/network/security-groups';
 import { RedisCluster } from './src/datastores/redis';
 import { mainVPC } from './src/network/vpc';
 import './src/network/route-tables';
@@ -62,7 +62,8 @@ import './src/network/route-tables';
             main: mainVPC
         },
         securityGroups: {
-            main: mainRedisAllowAllSG
+            main: mainRedisAllowAllSG,
+            mainSecurityGroup
         }
     };
 
